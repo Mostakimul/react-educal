223a0749782eaccc96cded3f109a5678cbed0332
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-04 18:04:40 +0200

    Error Page completed

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 
 const Error = () => {
+  let history = useHistory();
+
+  function handleClick() {
+    history.push('/home');
+  }
   return (
-    <div>
-      <h3>This is Error Page</h3>
+    <div className="container mx-auto">
+      <div className="text-center my-12 py-12">
+        <img src="/images/error.png" alt="Error" className="mx-auto" />
+        <h2 className="text-5xl font-semibold py-2">Page Not Found!</h2>
+        <p>Please try searching for some other page.</p>
+        <button
+          onClick={handleClick}
+          className="bg-green-700 text-white py-1 px-3 rounded-md mt-3 shadow-lg"
+        >
+          Back To Home
+        </button>
+      </div>
     </div>
   );
 };
