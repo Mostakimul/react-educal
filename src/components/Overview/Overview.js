c4a80deb426938964fd91345652dd3d99eeb9f04
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-04 16:39:09 +0200

    overview section completed

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -12,26 +12,31 @@ const Overview = () => {
           You don't have to struggle alone, you've got our assistance and help.
         </p>
       </div>
+      {/* Card Section */}
       <div className="grid grid-cols-4 gap-4 my-4">
         <OverviewCard
           className="bg-blue-600"
           title="4,000 Online courses"
           description="Arse over tit morish wind up grormless butty!"
+          ImBook="ImBook"
         ></OverviewCard>
         <OverviewCard
           className="bg-pink-600"
           title="Job plcament support"
           description="Arse over tit morish wind up grormless butty!"
+          ImMug="ImMug"
         ></OverviewCard>
         <OverviewCard
           className="bg-yellow-600"
           title="Lifetime Slack chat support"
           description="Arse over tit morish wind up grormless butty!"
+          ImAccessibility="ImAccessibility"
         ></OverviewCard>
         <OverviewCard
           className="bg-green-600"
           title="Research and innovation"
           description="Arse over tit morish wind up grormless butty!"
+          ImLeaf="ImLeaf"
         ></OverviewCard>
       </div>
     </div>
--- src/components/OverviewCard/OverviewCard.js
+++ src/components/OverviewCard/OverviewCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { ImBook } from 'react-icons/im';
 
 const OverviewCard = (props) => {
   return (
-    <div className={`text-white rounded-lg p-4 ${props.className}`}>
-      <h4 className="text-xl font-bold my-4">{props.title}</h4>
+    <div className={`text-white rounded-lg p-5 shadow-lg ${props.className}`}>
+      <ImBook size={50} />
+      <h4 className="text-xl font-bold my-6">{props.title}</h4>
       <p className="font-light">{props.description}</p>
     </div>
   );
