4979cf4692522dbbacaa23b47f363de12690022f
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-04 18:12:01 +0200

    All Services course page completed

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Courses from '../components/Courses/Courses';
 
 const Services = () => {
   return (
-    <div>
-      <h3>This is Services Page</h3>
+    <div className="bg-green-700">
+      <div className="container mx-auto text-white">
+        <h2 className="text-6xl font-semibold py-10">All Courses</h2>
+      </div>
+      <section className="bg-gray-200 py-12">
+        <Courses></Courses>
+      </section>
     </div>
   );
 };
